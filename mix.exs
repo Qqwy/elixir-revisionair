b28d067ddd848919bea59baee739cb22045ce98c
Qqwy/W-M at Quinn <qqwy@gmx.com>
2017-04-24 17:30:19 +0200

    increase number in mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Revisionair.Mixfile do
 
   def project do
     [app: :revisionair,
-     version: "0.10.0",
+     version: "0.11.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
